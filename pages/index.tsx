11f26f5a4a34b45ba382d32944b0ea864eac027a
Nao <45124890+NowNewNao@users.noreply.github.com>
2021-10-16 18:23:07 +0900

    Update title and link

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://github.com/NowNewNao/NowNewNao" target={"_blank"}>My Website</a>
         </h1>
 
 
