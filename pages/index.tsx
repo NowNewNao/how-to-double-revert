6a063c944ad96ac7c6b63e9582ff197dc7cfe9db
Nao <45124890+NowNewNao@users.noreply.github.com>
2021-10-16 19:15:42 +0900

    Revert "Merge pull request #1 from NowNewNao/feature/tech"
    This reverts commit 42ad90c64d3193ef79722b89e346f5f84675f563, reversing
    changes made to a1b935bd0d8d4b2226d58a74bb5bee71c827f337.
    
    # Conflicts:
    #	.idea/workspace.xml

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,9 @@ const Home: NextPage = () => {
             <h2>About &rarr;</h2>
           </a>
 
-          <a href="/tech" className={styles.card}>
-            <h2>Tech &rarr;</h2>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
